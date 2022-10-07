a6f0db9c2d52533e4736191200c2e370ec828480
Dario Carranza <dario_zmd@yahoo.com.ar>
2022-10-07 20:46:15 -0300

    Se logra secuencializar el reinicio.
    Esta incompleto el tratamiento del log principal.

--- reiniciador.sh
+++ reiniciador.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+verifica_log(){
+if test -f "./reiniciadorlog.log"; then
+  touch reiniciador.log
+fi
+}
+
+
+
+#verifica_log
 printf "DATOS: sip.conf\n"
 while read sip;
 do
-    echo "$sip"
+    echo $sip;
+    s=${sip%%:*};
+    ip=${sip##*\:};
+    #echo $s
+    #echo $ip
+    echo "Reinciando $s con ip: $ip";
+    $(ssh -i ./pkey/$s reiniciador@$ip "sudo /sbin/reboot") &> ./reiniciadorlog.log
+    wait
 done <sip.conf
 
-echo "Reinciando s1..."
+#echo "Reinciando s1..."
 #ssh -i ./pkey/s1 reiniciador@192.168.10.230 "sudo /sbin/reboot"
-echo "Reinciando s2..."
+#echo "Reinciando s2..."
 #ssh -i ./pkey/s2 reiniciador@172.16.22.230 "sudo /sbin/reboot"
-echo "Reinciando s3..."
+#echo "Reinciando s3..."
 #ssh -i ./pkey/s3 reiniciador@172.16.22.231 "sudo /sbin/reboot"
 echo "LISTO"
