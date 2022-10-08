777719c34bbd928932e54804ea1c787e913d39fd
Dario Carranza <dario_zmd@yahoo.com.ar>
2022-10-08 18:37:26 -0300

    Se agrega la verificación de estado de conexión con cada servidor declarado en el sip.conf.

--- reiniciador.sh
+++ reiniciador.sh
@@ -1,14 +1,50 @@
 #!/bin/bash
 
-verifica_log(){
-if test -f "./reiniciadorlog.log"; then
-  touch reiniciador.log
+verifica_archivos(){
+if ! test -f "./reiniciadorlog.log"; then
+  echo "creando reniciadorlog.log"
+  touch reiniciadorlog.log
+fi
+if ! test -f "./sip.conf"; then
+  echo "creando sip.conf"
+  touch sip.conf
+  echo "s1:xx.xx.xx.xx" | tee -a ./sip.conf
+  echo "Archivo sip.conf sin setear, ingrese los datos respetando la nomenclatura y vuelva a intentarlo"
+  exit
 fi
 }
 
+op_confirma(){
+  while true; do
+    read -p "Desea Continuar S/N?" op
+    case $op in
+      [Ss]* ) echo "Continua la Ejecución. !"; break;;
+      [Nn]* ) echo "Finaliza la Ejecución. !"; exit;;
+          * ) echo "Seleccione Si o No.";;
+    esac
+done
+}
 
+verificar_estado(){
+  while read sip;
+  do
+      echo $sip;
+      s=${sip%%:*};
+      ip=${sip##*\:};
+      #echo $s
+      #echo $ip
+      echo "Verificando estado de $s con ip: $ip";
+      if ping -c 1 $ip; then
+          continue
+      else
+          echo "servidor inaccesible!"
+          #op_confirma;
+      fi
+  done <sip.conf
+}
 
-#verifica_log
+verifica_archivos
+verificar_estado
 printf "DATOS: sip.conf\n"
 while read sip;
 do
@@ -18,14 +54,9 @@ do
     #echo $s
     #echo $ip
     echo "Reinciando $s con ip: $ip";
-    $(ssh -i ./pkey/$s reiniciador@$ip "sudo /sbin/reboot") &> ./reiniciadorlog.log
+    $(ssh -i ./pkey/$s reiniciador@$ip "sudo /sbin/reboot") &> reiniciadorlog.log
     wait
+
 done <sip.conf
 
-#echo "Reinciando s1..."
-#ssh -i ./pkey/s1 reiniciador@192.168.10.230 "sudo /sbin/reboot"
-#echo "Reinciando s2..."
-#ssh -i ./pkey/s2 reiniciador@172.16.22.230 "sudo /sbin/reboot"
-#echo "Reinciando s3..."
-#ssh -i ./pkey/s3 reiniciador@172.16.22.231 "sudo /sbin/reboot"
 echo "LISTO"
